34a6dcf5f902b061a17edba4c54325dc5935ba04
Nash Brancic <nashfft@gmail.com>
2019-03-08 04:26:28 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,6 +1,6 @@
 #status = "Hooray! Mario has rescued Princess Peach."
 
 def rescue_princess_peach(status)
-  status = "Hooray! Mario has rescued Princess Peach.
+  status = "Hooray! Mario has rescued Princess Peach."
   puts status
 end
